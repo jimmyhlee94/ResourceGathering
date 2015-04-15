19d5ef7025147465240430033343a240671428b8
jimmyhlee94 <jimmyhlee94@gmail.com>
2015-04-15 16:56:18 -0500

    edited params

--- src/resourceGathering/ResourceGatheringBuilder.java
+++ src/resourceGathering/ResourceGatheringBuilder.java
@@ -75,7 +75,7 @@ public class ResourceGatheringBuilder implements ContextBuilder<Object> {
 				
 		for (int j = 0; j < resourceCount; j++) {
 			//Resource with random value between 10-1000, inclusive and size of 1.
-			context.add(new Resource(space, grid, RandomHelper.nextIntFromTo(10,1000), 2, j));
+			context.add(new Resource(space, grid, RandomHelper.nextIntFromTo(10,1000), RandomHelper.nextIntFromTo(1, robotCount), j));
 		}
 
 		for (Object obj : context) {
